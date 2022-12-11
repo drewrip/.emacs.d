c433b7e0a490353787e8880a377979e75cb065f4
Drew Ripberger <drew.ripberger@gmail.com>
2022-12-11 04:11:08 -0500

    started fresh with doom theme and treemacs

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # .emacs.d
 **Emacs configuration file**
 
-Note that the `themes/` directory is not currently in use. It stores my original oceanic theme, but I have switched to solarized-dark. It remains incase I decide to switch back.
+I've recently started rewriting my config file for my current needs
+while also scrapping some of the copy pasting I did for my first file.
 
+Currently the config relies on all-the-icons because of treemacs, so you must
+also run `M-x all-the-icons-install-fonts`.
 
+My goal is to get LSP working so I can make emacs a bit more practical for my purposes.
+Part of this will be setting up Rust Analyzer in emacs. See https://robert.kra.hn/posts/rust-emacs-setup/.
--- init.el
+++ init.el
@@ -6,46 +6,98 @@
 
 ;;; Code:
 
-;; Increase the garbage collection threshold to 500 MB to ease startup
-(setq gc-cons-threshold (* 500 1024 1024))
-
 ;; List package archives and initialize them
 (require 'package)
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
-;; Make sure Org is installed
-(unless (package-installed-p 'org)
-  (package-refresh-contents)
-  (package-install 'org))
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 ;; Make sure use-package is installed
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; Org plus contrib needs to be loaded before any org related functionality is called
-(unless (package-installed-p 'org-plus-contrib)
-  (package-refresh-contents)
-  (package-install 'org-plus-contrib))
-;; Load config.org - my Emacs configuration
-(org-babel-load-file (concat user-emacs-directory "config.org"))
 
-;; Garbage collector - decrease threshold to 5 MB
-(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold (* 5 1024 1024))))
-;;; init.el ends here
+;; Simple UI Tweaks => Scroll bar, tool bar, etc
+;; Do not show the startup screen.
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(setq initial-scratch-message "") ; When opening a new buffer, don't show the scratch message.
+(setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message t)
+(setq ring-bell-function 'ignore)
+
+;; Highlight current line.
+(global-hl-line-mode t)
+
+;; Show line numbers
+(global-linum-mode 1)
+
+;; All the icons dependency for treemacs
+(use-package all-the-icons
+  :ensure t
+  :if (display-graphic-p))
+
+;; Treemacs for file navigation
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t d"   . treemacs-select-directory)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(with-eval-after-load 'doom-themes
+  (doom-themes-treemacs-config))
+
+;; Theme
+;; Using the Doom Colors theme from Doom Emacs
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+          
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  (setq doom-themes-treemacs-theme "doom-colors") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+
+;; Programming and Markup Languages!
+
+;; Rust
+;; Go
+;; C/C++
+;; Python
+;; Markdown
+;; YAML
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org-plus-contrib))))
+ '(package-selected-packages (quote (doom-themes use-package org-plus-contrib))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
